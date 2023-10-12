7ad8aed9ee7dfe6145f14256eda6a74b11967d4a
Santiago Celis <celisgil.santiago.101@gmail.com>
2023-10-12 17:52:20 -0500

    Login parte 3

---
+++ LOGIN/cliente/src/pages/RegisterPage.jsx
@@ -0,0 +1,29 @@
+import { useForm } from 'react-hook-form';
+import { registerRequest } from '../api/auth';
+
+
+function RegisterPage() {
+
+    const { register, handleSubmit } = useForm();
+
+  return (
+    <div className='bg-zinc-600 max-w-md p-10 rounded-md'>
+      <form onSubmit={handleSubmit(async (values) => {
+        console.log(values);
+        const res = await registerRequest(values);
+        console.log(res)
+      })} >
+        <input type="text" {...register("username", {required:true})} placeholder='username' 
+           className='w-full bg-zinc-400 text-white px-4 py-4 rounded-md my-2' />
+        <input type="email" {...register("email", {required:true})} placeholder='email'  
+           className='w-full bg-zinc-400 text-white px-4 py-4 rounded-md my-2' />
+        <input type="password" {...register("password", {required:true})} placeholder='password' 
+            className='w-full bg-zinc-400 text-white px-4 py-4 rounded-md my-2' />
+        <button type="submit" className='bg-blue-500 hover:bg-slate-700 text-white font-bold py-2 
+           px-4  rounded-full' >Register</button>
+      </form>
+    </div>
+  )
+}
+
+export default RegisterPage
